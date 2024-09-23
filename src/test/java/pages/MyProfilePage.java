01ea03ca3253f63019679680a5d6a2323a041e8f
Admin <savenokslava@gmail.com>
2024-09-23 21:39:32 +0300

    upload

--- src/test/java/pages/MyProfilePage.java
+++ src/test/java/pages/MyProfilePage.java
@@ -4,13 +4,14 @@ import baseEntities.BasePage;
 import elements.Button;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import services.WaitsService;
 
 public class MyProfilePage extends BasePage {
 
     private final By PAGE_LOCATOR = By.xpath("//h3[text() = 'My profile']");
     private final By UPLOAD_AVATAR_BUTTON_LOCATOR=By.xpath("//div[text()='Upload avatar']");
     private final By SAVE_AVATAR_BUTTON_LOCATOR=By.xpath("//div[text()='Save']");
-
 
     public MyProfilePage(WebDriver driver) {
         super(driver);
--- src/test/java/test/gui/FileUploadTest.java
+++ src/test/java/test/gui/FileUploadTest.java
@@ -7,23 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class FileUploadTest extends BaseTest {
 
     private static final Logger log = LoggerFactory.getLogger(FileUploadTest.class);
 
     @Test
-    public void fileUploadTest() throws InterruptedException {
+    public void fileUploadTest() {
         userStep.successfulLogin();
         dashboardPage.chooseSettingsDropDown();
-        WebElement fileUploadElement = waitsService.presenceOfElementLocated(By.xpath("//*[@accept='image/jpeg, image/png, image/gif, image/webp, image/svg+xml']"));
-       // String pathToFile="D:/Project/AQA27ONLDiplomaGroup_1/src/test/resources/data/dataForFileUploadTest/profile_avatar.jpg";
-        String pathToFile = FileUploadTest.class.getClassLoader().getResource("data/dataForFileUploadTest/profile_avatar.jpg").getPath();
-        System.out.println(pathToFile);
+        WebElement fileUploadElement = waitsService.presenceOfElementLocated(By.xpath("//input[@style='display: none;']"));
+        String pathToFile = FileUploadTest.class.getClassLoader().getResource("data" + File.separator + "dataForFileUploadTest" + File.separator + "profileAvatar.jpg").getPath().replace("/D", "D").replace("%5c", "/");
         fileUploadElement.sendKeys(pathToFile);
-        // myProfilePage.clickUploadAvatar();
         myProfilePage.clickSaveAvatar();
-        //Assert.assertEquals(driver.findElement(By.id("uploaded-files")).getText(), "sample-zip-file.zip");
-
-
     }
 }
