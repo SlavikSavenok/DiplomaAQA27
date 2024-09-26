e9b26c248f888c3d5b2412199c7677c889bbb57a
DaniilLugivsky <propro.organav@gmail.com>
2024-09-26 20:36:59 +0300

    Commit

--- src/test/java/utils/endpoints/Endpoints.java
+++
@@ -1,9 +0,0 @@
-package utils.endpoints;
-
-public class Endpoints {
-    public static final String GET_PROJECT = "api/v1/project/{id}";
-    public static final String GET_PROJECTS = "api/v1/project";
-    public static final String ADD_PROJECT = "api/v1/project";
-    public static final String DELETE_PROJECT = "api/v1/project/{id}";
-    public static final String UPDATE_PROJECT = "api/v1/project/{id}";
-}
